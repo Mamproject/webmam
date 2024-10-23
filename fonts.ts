8545922cf0ac0a85b4745ac45718eb323b86aaeb
Mamproject <mamprojectukunda@gmail.com>
2024-10-24 00:05:38 +0200

    Delete fonts.ts

--- fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Antonio, Montserrat, Open_Sans } from "next/font/google";
-
-export const antonio = Antonio({ subsets: ["latin"], variable: "--font-antonio" });
-export const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-montserrat" });
-export const openSans = Open_Sans({ subsets: ["latin"], variable: "--font-open-sans" });
