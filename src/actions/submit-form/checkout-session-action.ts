16f8dbf18a775662e9df5cef8783d127305ca846
Mamproject <mamprojectukunda@gmail.com>
2024-10-23 23:29:54 +0200

    Add files via upload

---
+++ src/actions/submit-form/checkout-session-action.ts
@@ -0,0 +1,90 @@
+"use server";
+
+import { getDictionary } from "@/i18n/get-dictionary";
+import type { Locale } from "@/i18n/i18n-config";
+import { stripe } from "@/lib/stripe";
+import type { TCheckoutFormWithMeta } from "@/utils/form-schemas";
+import { CheckoutFormSchemaWithMeta } from "@/utils/form-schemas";
+import { Value } from "@sinclair/typebox/value";
+import type Stripe from "stripe";
+
+type ActionResponse =
+  | {
+      status: "success";
+      sessionId: string;
+    }
+  | {
+      status: "error";
+    };
+
+export async function checkoutSessionAction(formData: TCheckoutFormWithMeta): Promise<ActionResponse> {
+  try {
+    const body = validateSchema(formData);
+    const cancelSearchParams = new URLSearchParams({
+      initialView: "form",
+      amount: (formData.amount / 100).toString(),
+      frequency: formData.frequency,
+      terms: "true",
+    }).toString();
+
+    const dictionary = await getDictionary(body.locale as Locale);
+    const mode = body.frequency === "one-time" ? "payment" : "subscription";
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "eur",
+            product_data: {
+              name: dictionary.violet_bricks,
+              description: dictionary.violet_bricks_description,
+            },
+            unit_amount: body.amount,
+            recurring: getRecurringSettings(body.frequency),
+          },
+          quantity: 1,
+        },
+      ],
+      mode,
+      success_url: `${body.origin}/${body.locale}/join?initialView=success`,
+      cancel_url: `${body.origin}/${body.locale}/join?${cancelSearchParams}`,
+      invoice_creation: mode === "payment" ? { enabled: true } : undefined,
+      locale: body.locale as Locale,
+    });
+    return {
+      status: "success",
+      sessionId: session.id,
+    };
+  } catch (error) {
+    return { status: "error" };
+  }
+}
+
+const validateSchema = (body: unknown) => {
+  const valid = Value.Check(CheckoutFormSchemaWithMeta, body);
+  if (!valid) throw new Error("Invalid form");
+  return body as TCheckoutFormWithMeta;
+};
+
+const getRecurringSettings = (
+  frequency: TCheckoutFormWithMeta["frequency"],
+): Stripe.Checkout.SessionCreateParams.LineItem.PriceData.Recurring | undefined => {
+  switch (frequency) {
+    case "one-time":
+      return undefined;
+    case "monthly":
+      return {
+        interval: "month",
+        interval_count: 1,
+      };
+    case "quarterly":
+      return {
+        interval: "month",
+        interval_count: 3,
+      };
+    case "yearly":
+      return {
+        interval: "year",
+        interval_count: 1,
+      };
+  }
+};
---
+++ src/actions/submit-form/submit-form-action.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import type { TFormData } from "@/hooks/use-send-form";
+import type { TForm, TFormKeys, TSentForm } from "@/utils/form-schemas";
+import { FormSchemas, SentFormSchema } from "@/utils/form-schemas";
+import { Value } from "@sinclair/typebox/value";
+import { sendEmail } from "./aux/send-email";
+import { validateCaptcha } from "./aux/validate-captcha";
+import { grecaptchaActions } from "@/settings/grecaptcha-actions";
+
+interface ContactFormStatus {
+  status: "success" | "error";
+}
+
+export async function submitFormAction(formData: TFormData): Promise<ContactFormStatus> {
+  try {
+    const body = validateSchema(formData);
+    await validateCaptcha(body.token, grecaptchaActions.contactForm);
+    await sendEmail(body as TSentForm & TForm[TFormKeys]);
+    return { status: "success" };
+  } catch (error) {
+    return { status: "error" };
+  }
+}
+
+const validateSchema = (body: unknown) => {
+  const validMeta = Value.Check(SentFormSchema, body);
+  if (!validMeta) throw new Error("Invalid form");
+
+  const { formKey } = body as TSentForm;
+  const validData = Value.Check(FormSchemas[formKey], body);
+  const { terms } = Value.Cast(FormSchemas[formKey], body);
+  if (!validData || !terms) throw new Error("Invalid form");
+
+  return body as TSentForm;
+};
