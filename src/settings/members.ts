a3f31d92a57e3e35f68e161200d334480880f42d
Mamproject <mamprojectukunda@gmail.com>
2024-12-07 14:54:15 +0100

    Update members.ts

--- src/settings/members.ts
+++ src/settings/members.ts
@@ -1,13 +1,12 @@
 import type { MemberCardProps } from "@/components/MemberCard";
 import picPlaceholder from "@/assets/pic_placeholder.png";
 import type { Dictionary } from "@/i18n/dictionaries/es";
-import MartaFalguera from "@/assets/MartaSillaRodas.JPG";
 
 export const members = (dictionary: Dictionary): MemberCardProps[] => [
   {
     name: "Marta Falguera",
     title: dictionary.president,
-    pictureSrc: MartaFalguera,
+    pictureSrc: picPlaceholder,
   },
   {
     name: "Ángela Suárez",
