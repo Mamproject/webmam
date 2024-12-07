99cd3ca83b4de506e2fc59b2e54c3d447533e1f4
Mamproject <mamprojectukunda@gmail.com>
2024-12-07 12:38:35 +0100

    Update members.ts

--- src/settings/members.ts
+++ src/settings/members.ts
@@ -5,18 +5,37 @@ import type { Dictionary } from "@/i18n/dictionaries/es";
 export const members = (dictionary: Dictionary): MemberCardProps[] => [
   {
     name: "Marta Falguera",
-    title: dictionary.president,
-    pictureSrc: picPlaceholder,
-  },
-  {
-    name: "Ángela Suárez",
     title: dictionary.secretary,
     pictureSrc: picPlaceholder,
   },
   {
-    name: "Miguel Sánchez",
+    name: "Ángela Suárez",
     title: dictionary.treasurer,
     pictureSrc: picPlaceholder,
   },
-  ,
+  {
+    name: "Marina Riera",
+    title: dictionary.president,
+    pictureSrc: picPlaceholder,
+  },
+  {
+    name: "Abdul Maqbul",
+    title: dictionary.kenya_manager,
+    pictureSrc: picPlaceholder,
+  },
+  {
+    name: "Leah Wambu",
+    title: dictionary.kenya_manager,
+    pictureSrc: picPlaceholder,
+  },
+  {
+    name: "Mama Gladys",
+    title: dictionary.kenya_manager,
+    pictureSrc: picPlaceholder,
+  },
+  {
+    name: "Daniel Mateos",
+    title: dictionary.web_development,
+    pictureSrc: picPlaceholder,
+  },
 ];
