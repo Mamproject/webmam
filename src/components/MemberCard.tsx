3bd6d02f6e61fd5edf1e095c75c78997896ce499
Mamproject <mamprojectukunda@gmail.com>
2024-12-11 23:53:19 +0100

    Update MemberCard.tsx

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -10,8 +10,8 @@ export interface MemberCardProps {
 }
 
 const MemberCard: FC<MemberCardProps> = ({ name, title, pictureSrc }) => (
-  <article className="flex flex-col items-center gap-1">
-    <div className="h-64 w-64 overflow-hidden rounded-full bg-purple">
+  <article className="flex flex-col items-center gap-2">
+    <div className="h-48 w-48 md:h-64 md:w-64 overflow-hidden rounded-full bg-purple">
       <Image src={pictureSrc} alt={name} width={256} height={256} className="object-cover object-center" />
     </div>
 
