918ec1a90e0e49d56961f04a69943bcf956f53f8
Mamproject <mamprojectukunda@gmail.com>
2024-12-12 23:48:09 +0100

    Update socialMedia.ts

--- src/settings/socialMedia.ts
+++ src/settings/socialMedia.ts
@@ -1,5 +1,6 @@
 import twitterLogo from "@/assets/twitter.png";
 import instagramLogo from "@/assets/instagram.png";
+import facebookLogo from "@/assets/facebook_logo.png";
 
 export const socialMediaData = {
   twitter: {
@@ -10,9 +11,13 @@ export const socialMediaData = {
     logo: instagramLogo,
     url: process.env.NEXT_PUBLIC_INSTAGRAM_URL,
   },
+  facebook: {
+    logo: facebookLogo,
+    url: process.env.NEXT_PUBLIC_FACEBOOK_URL,
+  },
 };
 
 export type SocialMediaType = keyof typeof socialMediaData;
 
 // This is done to preserve the order of social media items when we iterate over them
-export const socialMediaKeys: SocialMediaType[] = ["instagram"];
+export const socialMediaKeys: SocialMediaType[] = ["instagram", "facebook"];
